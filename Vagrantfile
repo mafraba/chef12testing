902c3d8305de8dca5e77725488be736c85d23e80
Manuel Franco <mfranco@flexiant.com>
2015-05-20 17:03:38 +0200

    server bootstrap

--- Vagrantfile
+++ Vagrantfile
@@ -11,6 +11,11 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     server.vm.box_url = 'https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box'
     server.vm.hostname = "aserver"
     server.vm.network "private_network", ip: "192.168.33.11"
+    server.vm.provision :shell, path: "server-bootstrap.sh"
+    server.vm.provider :virtualbox do |vb|
+        vb.customize ["modifyvm", :id, "--memory", "4096"]
+        vb.customize ["modifyvm", :id, "--cpus", "2"]
+    end
   end
 
   config.vm.define "aworks" do |ws|
---
+++ server-bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get upgrade -y
+sudo locale-gen es_ES.UTF-8
+
+# Ensure that the Server is Accessible by Hostname
+sudo echo "127.0.1.1 192.168.33.11 aserver" > /etc/hosts
+sudo echo "127.0.0.1 localhost" >> /etc/hosts
+sudo echo "192.168.33.11 192.168.33.11 aserver" >> /etc/hosts
+
+# Get and install Chef server
+wget https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/chef-server-core_12.0.8-1_amd64.deb
+sudo dpkg -i chef-server-core_*.deb
+sudo chef-server-ctl reconfigure
+
+# Create an Admin User and Organization
+sudo chef-server-ctl user-create admin admin admin admin@example.com examplepass -f admin.pem
+sudo chef-server-ctl org-create acme "ACME" --association_user admin -f acme-validator.pem
+
+# Copy pivotal.pem to home, so that we can easily retrieve it from the workstation by scp
+sudo cp /etc/opscode/pivotal.pem ~/
