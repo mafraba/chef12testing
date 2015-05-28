fadeb481286232c2b7bbd2d33319a6068efe510e
Manuel Franco <mfranco@flexiant.com>
2015-05-28 17:29:09 +0200

    Testing code updated

--- code/cheftest.rb
+++ code/cheftest.rb
@@ -1,4 +1,6 @@
 require 'rubygems'
+require 'chef'
+require 'chef/user'
 require 'chef/config'
 require 'chef/log'
 require 'chef/rest'
@@ -10,12 +12,12 @@ signing_key_filename="/vagrant/pivotal.pem"
 rest = Chef::REST.new(chef_server_url, client_name, signing_key_filename)
 
 # Organizations list
-puts "Listing organizations: "
+puts "\n* Listing organizations: "
 puts orgs = rest.get_rest("/organizations")
 
 # Create organization
 if not orgs['frabs']
-  puts "Creating organization: "
+  puts "\n* Creating organization: "
   puts rest.post(
     '/organizations', 
     { name: "frabs", full_name: "Frabs, Org."}
@@ -23,10 +25,62 @@ if not orgs['frabs']
 end
 
 # Get organization
-puts "Getting a single organization: "
+puts "\n* Getting a single organization: "
 puts rest.get("/organizations/frabs")
 
+
+# Users list
+puts "\n* Listing users: "
+puts users = rest.get_rest("/users")
+
+# Get user details
+puts "\n* Getting user details: "
+puts rest.get("/users/admin")
+
+
+# Create user
+if not users['mafraba']
+  puts "\n* Creating user: "
+  puts rest.post(
+    '/users', 
+    { 
+      name: "mafraba",
+      display_name: 'Manuel Franco',
+      email: 'mafraba@gmail.com',
+      admin: true,
+      password: 'Saluda.123'
+    }
+  )
+  # user = Chef::User.new
+  # user.name('mafraba')
+  # user.admin(true)
+  # user.password 'Saluda.123'
+  # user.create
+end
+
+
+puts "\n* Listing users for organization: "
+puts rest.get('/organizations/acme/users')
+
+puts "\n* Adding admin for organization: "
+puts rest.post(
+  '/organizations/frabs/groups/admins/users', 
+  { 
+    username: "mafraba"
+  }
+)
+
+puts "\n* Listing admins for organization: "
+puts rest.get('/organizations/acme/groups/admins')
+
+################### CLEAN UP ###########################
+
+# Delete user
+puts "\n* Deleting a user: "
+puts rest.delete("/users/mafraba")
+
+
 # Delete organization
-puts "Deleting an organization: "
+puts "\n* Deleting an organization: "
 puts rest.delete("/organizations/frabs")
 
